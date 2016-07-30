5642c25ec75f991bbfb1ee3021bec315af90c4e6
root <jeff@1.local>
2016-07-30 03:34:00 -0700

    Data pushes through backend. Next is character recognition

---
+++ README.md
@@ -0,0 +1,6 @@
+# Mass YouTube renamer
+
+Node, ExpressJS
+
+# Usage
+YouTube mass file renamer based on text found in thumbnail. For GimR
--- routes/youtubestuff.js
+++ routes/youtubestuff.js
@@ -7,8 +7,21 @@ router.get('/', function (req, res) {
   res.send('Do authenticate');
 });
 
-router.get('/videos', function (req, res) {
-  res.send('Got a POST request');
+router.post('/yt_thumbnails_receive', function (req, res) {
+  var videoJson = req.body;
+  console.log("[D] Received thumbnails data. Key count:", Object.keys(req.body).length);
+
+
+  var json = {};
+
+  for(var i in videoJson) {
+    json[i] = videoJson[i];//videoJson[i] ...
+  }
+
+
+
+
+  res.json(json);
 });
 
 router.post('/changenames', function (req, res) {
